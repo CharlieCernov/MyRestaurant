9b3aaa3bb6f20fb19f32c5b87877725b9225353a
CharlieCernov <charlie.fay21@gmail.com>
2022-08-16 11:50:22 -0300

    Initial commit

---
+++ app/src/main/java/br/com/myrestaurant/ActivityJanelaPedido.java
@@ -0,0 +1,14 @@
+package br.com.myrestaurant;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityJanelaPedido extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_janela_pedido);
+    }
+}
