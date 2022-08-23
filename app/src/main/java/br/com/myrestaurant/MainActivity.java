d5a0cc82222be6911e141865e61e59fc5c524692
CharlieCernov <charlie.fay21@gmail.com>
2022-08-23 11:54:24 -0300

    Atualizando...

--- app/src/main/java/br/com/myrestaurant/MainActivity.java
+++ app/src/main/java/br/com/myrestaurant/MainActivity.java
@@ -1,14 +1,23 @@
 package br.com.myrestaurant;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+   private TextView teste1;
+   private TextView teste2;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        teste1=findViewById(R.id.preco);
+        teste2=findViewById(R.id.valuetotal);
+
+        teste1+teste2
     }
 }
